72a189d27a3d9ccca9af74b7cd5168252300b7a6
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-09-23 00:45:03 +0800

    feat: adjust title

--- src/app.tsx
+++ src/app.tsx
@@ -2,14 +2,16 @@ import { Header } from './components/header'
 
 function App() {
   return (
-    <div className="min-h-svh">
+    <div className="min-h-svh space-y-600 lg:space-y-800">
       <div className="mx-auto box-content max-w-[1216px] px-200 sm:px-300">
         <Header />
       </div>
 
-      <h1 className="text-preset-2 font-bricolage-grotesque">
-        How&rsquo;s the sky looking today?
-      </h1>
+      <div className="mx-auto box-content px-200 max-lg:max-w-[482px] sm:px-300">
+        <h1 className="text-preset-2 font-bricolage-grotesque text-center">
+          How&rsquo;s the sky looking today?
+        </h1>
+      </div>
     </div>
   )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import { UnitsDropdown } from './units-dropdown'
 
 export function Header() {
   return (
-    <header className="flex justify-between py-200 sm:py-300 xl:py-600">
+    <header className="flex justify-between pt-200 sm:pt-300 lg:pt-600">
       <a href="/">
         <Logo className="h-[28px] sm:h-500" aria-hidden="true" />
         <span className="sr-only">Weather Now</span>
