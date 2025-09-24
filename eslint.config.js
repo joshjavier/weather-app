ab70be9f36838ade2d4cc8b3ae09aae5908747f3
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-09-24 12:22:24 +0800

    chore: install ESLint Plugin Query

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import pluginQuery from '@tanstack/eslint-plugin-query'
+import eslintConfigPrettier from 'eslint-config-prettier'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
-import eslintConfigPrettier from 'eslint-config-prettier'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -15,6 +16,7 @@ export default tseslint.config([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      ...pluginQuery.configs['flat/recommended'],
       eslintConfigPrettier,
     ],
     languageOptions: {
