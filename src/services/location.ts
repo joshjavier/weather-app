a519dbabba9321e6ea70b431f56e8bcb21475731
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-09-24 17:01:47 +0800

    feat: create a fetcher for locations data

---
+++ src/services/location.ts
@@ -0,0 +1,54 @@
+// Open-Meteo's Geocoding API accepts a search term and returns a list
+// of matching locations. See https://open-meteo.com/en/docs/geocoding-api
+
+type Location = Record<string, string | number> & {
+  id: number
+  name: string
+  latitude: number
+  longitude: number
+  elevation: number
+  timezone: string
+  feature_code: string
+  country_code: string
+  country: string
+  country_id: string
+  population: number
+  postcodes: string[]
+}
+
+type LocationData = Location[]
+
+type LocationError = {
+  error: true
+  reason: string
+}
+
+function isLocationError(value: unknown): value is LocationError {
+  return (
+    typeof value === 'object' &&
+    value !== null &&
+    'error' in value &&
+    'reason' in value
+  )
+}
+
+export async function fetchLocations(
+  searchTerm: string,
+): Promise<LocationData> {
+  const apiUrl = 'https://geocoding-api.open-meteo.com/v1/search'
+  const params = new URLSearchParams()
+  params.append('name', searchTerm)
+
+  const response = await fetch(`${apiUrl}?${params}`)
+  const result = await response.json()
+
+  if (!response.ok) {
+    let message = 'Failed to fetch locations'
+    if (isLocationError(result)) {
+      message += `: ${result.reason}`
+    }
+    throw new Error(message)
+  }
+
+  return result
+}
