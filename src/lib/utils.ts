a99192909e312f90cf47c0f904d9901db6793d58
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-10-04 06:10:12 +0800

    feat: update design tokens

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,22 @@
 import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { extendTailwindMerge } from 'tailwind-merge'
+
+const twMerge = extendTailwindMerge({
+  extend: {
+    theme: {
+      text: [
+        'preset-1',
+        'preset-2',
+        'preset-3',
+        'preset-4',
+        'preset-5',
+        'preset-6',
+        'preset-7',
+        'preset-8',
+      ],
+    },
+  },
+})
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
