076712f086333280847054349beabd511eca6552
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-10-04 14:24:49 +0800

    feat: build Title component

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,13 @@
 import { Header } from './components/header'
+import { Title } from './components/title'
 
 function App() {
   return (
     <div className="gap-800 pb-1000 flex min-h-svh flex-col">
       <Header className="pt-200 sm:pt-300 lg:pt-600" />
+      <main className="contents">
+        <Title />
+      </main>
     </div>
   )
 }
---
+++ src/components/title.tsx
@@ -0,0 +1,11 @@
+export function Title() {
+  return (
+    <div>
+      <div className="px-200 mx-auto box-content max-lg:max-w-[482px]">
+        <h1 className="text-preset-2 font-bricolage-grotesque text-center max-[325px]:text-[50px]">
+          How&rsquo;s the sky looking today?
+        </h1>
+      </div>
+    </div>
+  )
+}
