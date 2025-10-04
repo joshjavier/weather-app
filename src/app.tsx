3a10536a4144fed63a14273d8e6528f49e53b352
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-10-04 13:04:32 +0800

    feat: update button variants

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
+      <Button variant="trigger" size="sm">
+        Click me
+      </Button>
     </div>
   )
 }
